ed4cf1087ed88e51ee1ff010b1573454e84c1348
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-26 18:10:18 -0300

    Banco de dados

--- introduction-to-django/alurarecipes/settings.py
+++ introduction-to-django/alurarecipes/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurarecipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_recipe',
+        'USER': 'postgres',
+        'PASSWORD': 'M3ns3tM@nus',
+        'HOST': 'localhost'
     }
 }
 
