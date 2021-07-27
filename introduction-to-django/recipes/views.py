734c9174bfe0736077ffe5bef9b32da847253e82
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-26 23:10:56 -0300

    Parâmetros na url

--- introduction-to-django/recipes/views.py
+++ introduction-to-django/recipes/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
+from .models import Recipe
 
 def index(request):
-    receitas = {
-        1:'Sopa de Legumes',
-        2:'Sorvete',
-        3:'Lazanha',
-        4:'Bolo de chocolate'
-    }
+    receitas = Recipe.objects.all()
 
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
     
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Recipe, pk=receita_id)
+    receita_a_exibir = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_a_exibir)
