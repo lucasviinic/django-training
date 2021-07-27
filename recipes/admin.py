bda39f926a77af6d69b3b5b6767ca66f5f4b6622
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-27 19:54:25 -0300

    Ajustando Django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Recipe
 
-# Register your models here.
-admin.site.register(Recipe)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_da_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_da_receita')
+    search_fields = ('nome_da_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 5
+
+admin.site.register(Recipe, ListandoReceitas)
