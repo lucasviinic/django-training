1116f498b3a6008b898986780bd6c0ff5acdc70e
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-08-18 18:55:24 -0300

    Cadastro e Login

--- alurarecipes/settings.py
+++ alurarecipes/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'alurarecipes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'recipes/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
